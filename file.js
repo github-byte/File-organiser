0950b7509cf5d59b44cfc09b3ee42bafaf7916a8
Dipannita <dipannita223@gmail.com>
2021-03-12 17:49:04 +0530

    Automate Files

--- file.js
+++ file.js
@@ -17,6 +17,14 @@ fs.readdir(directoryPath, function (err, files) {
                     console.log("Text: "+file)
 
                     const pathToFile=directoryPath+"/"+file;
+                    
+                    fs.mkdir(path.join(__dirname,"music"),function(err){
+                        if(err){
+                            console.log(err)
+                        }
+                    
+                    })
+
                     const pathToNewDestination=directoryPath+"/music"+"/"+file
 
                 fs.rename(pathToFile, pathToNewDestination, function (err) {
@@ -28,6 +36,13 @@ fs.readdir(directoryPath, function (err, files) {
                 if(name[1]==='html'||name[1]==="css"||name[1]==="python"||name[1]==="java"){
                   
                             const pathToFile=directoryPath+"/"+file;
+                            fs.mkdir(path.join(__dirname,"code"),function(err){
+                                if(err){
+                                    console.log(err)
+                                }
+                            
+                            })
+        
                             const pathToNewDestination=directoryPath+"/code"+"/"+file
         
                         fs.rename(pathToFile, pathToNewDestination, function (err) {
@@ -41,6 +56,13 @@ fs.readdir(directoryPath, function (err, files) {
                     if(name[1]==='docx'||name[1]==='doc'){
                   
                         const pathToFile=directoryPath+"/"+file;
+                        fs.mkdir(path.join(__dirname,"docs"),function(err){
+                            if(err){
+                                console.log(err)
+                            }
+                        
+                        })
+    
                         const pathToNewDestination=directoryPath+"/docs"+"/"+file
     
                     fs.rename(pathToFile, pathToNewDestination, function (err) {
@@ -53,6 +75,13 @@ fs.readdir(directoryPath, function (err, files) {
                     if(name[1]==='gif'){
                   
                         const pathToFile=directoryPath+"/"+file;
+                        fs.mkdir(path.join(__dirname,"gifs"),function(err){
+                            if(err){
+                                console.log(err)
+                            }
+                        
+                        })
+
                         const pathToNewDestination=directoryPath+"/gifs"+"/"+file
     
                     fs.rename(pathToFile, pathToNewDestination, function (err) {
@@ -65,6 +94,13 @@ fs.readdir(directoryPath, function (err, files) {
                     if(name[1]==='jpeg'||name[1]==='PNG'||name[1]==='png'){
                   
                         const pathToFile=directoryPath+"/"+file;
+                        fs.mkdir(path.join(__dirname,"images"),function(err){
+                            if(err){
+                                console.log(err)
+                            }
+                        
+                        })
+    
                         const pathToNewDestination=directoryPath+"/images"+"/"+file
     
                     fs.rename(pathToFile, pathToNewDestination, function (err) {
@@ -74,9 +110,17 @@ fs.readdir(directoryPath, function (err, files) {
               
             
                     }
-                    if(name[1]==='zipped'){
+                    if(name[1]==='zip'){
                   
                         const pathToFile=directoryPath+"/"+file;
+                        fs.mkdir(path.join(__dirname,"zipped"),function(err){
+                            if(err){
+                                console.log(err)
+                            }
+                        
+                        })
+    
+                        
                         const pathToNewDestination=directoryPath+"/zipped"+"/"+file
     
                     fs.rename(pathToFile, pathToNewDestination, function (err) {
@@ -90,6 +134,13 @@ fs.readdir(directoryPath, function (err, files) {
                     if(name[1]==='mp4'){
                   
                         const pathToFile=directoryPath+"/"+file;
+                        fs.mkdir(path.join(__dirname,"videos"),function(err){
+                            if(err){
+                                console.log(err)
+                            }
+                        
+                        })
+    
                         const pathToNewDestination=directoryPath+"/videos"+"/"+file
     
                     fs.rename(pathToFile, pathToNewDestination, function (err) {
